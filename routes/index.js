8affa4977dfa31da689262d238e78c9594134fe1
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2024-10-15 10:04:00 +0800

    Data on Dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 const { Validator } = require("./controller/validator");
-const { SelectStatement, GetCurrentDate, GetCurrentMonthFirstDay, GetCurrentMonthLastDay } = require("./repository/helper");
+const {
+  SelectStatement,
+  GetCurrentDate,
+  GetCurrentMonthFirstDay,
+  GetCurrentMonthLastDay,
+} = require("./repository/helper");
 const { Select } = require("./repository/dbconnect");
 var router = express.Router();
 
@@ -27,11 +32,13 @@ router.get("/getdailysales", (req, res) => {
         console.error(error);
         return res.status(500).send({ msg: error });
       }
-      
+
       if (result.length != 0) {
         return res.status(200).send({ msg: "success", data: result[0] });
       } else {
-        return res.status(200).send({ msg: "success", data: result == null ? 0 : result });
+        return res
+          .status(200)
+          .send({ msg: "success", data: result == null ? 0 : result });
       }
     });
   } catch (error) {
@@ -50,7 +57,10 @@ router.get("/getmothlysales", (req, res) => {
       case when  isnull(sum(s_total)) then 0 else sum(s_total) end as total
       from sales
       where s_date between ? and ?`,
-      [`${GetCurrentMonthFirstDay()} 00:00:00`, `${GetCurrentMonthLastDay()} 23:59:59`]
+      [
+        `${GetCurrentMonthFirstDay()} 00:00:00`,
+        `${GetCurrentMonthLastDay()} 23:59:59`,
+      ]
     );
 
     Select(sql, (error, result) => {
@@ -58,11 +68,13 @@ router.get("/getmothlysales", (req, res) => {
         console.error(error);
         return res.status(500).send({ msg: error });
       }
-      
+
       if (result.length != 0) {
         return res.status(200).send({ msg: "success", data: result[0] });
       } else {
-        return res.status(200).send({ msg: "success", data: result == null ? 0 : result });
+        return res
+          .status(200)
+          .send({ msg: "success", data: result == null ? 0 : result });
       }
     });
   } catch (error) {
@@ -72,7 +84,6 @@ router.get("/getmothlysales", (req, res) => {
     });
   }
 });
-
 
 router.get("/getorders", (req, res) => {
   try {
@@ -86,11 +97,38 @@ router.get("/getorders", (req, res) => {
         console.error(error);
         return res.status(500).send({ msg: error });
       }
-      
+
       if (result.length != 0) {
         return res.status(200).send({ msg: "success", data: result[0] });
       } else {
-        return res.status(200).send({ msg: "success", data: result == null ? 0 : result });
+        return res
+          .status(200)
+          .send({ msg: "success", data: result == null ? 0 : result });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      msg: error,
+    });
+  }
+});
+
+router.get("/getnewusers", (req, res) => {
+  try {
+    let sql = "SELECT COUNT(*) as total FROM sims.customer";
+    Select(sql, (error, result) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send({ msg: error });
+      }
+
+      if (result.length != 0) {
+        return res.status(200).send({ msg: "success", data: result[0] });
+      } else {
+        return res
+          .status(200)
+          .send({ msg: "success", data: result == null ? 0 : result });
       }
     });
   } catch (error) {
